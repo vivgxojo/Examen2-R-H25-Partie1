cbff178113706fc37384fbd4bd113a6ee899d716
Maryse M <Maryse.Mongeau@cegepoutaouais.qc.ca>
2025-03-04 11:42:15 -0500

    Compléter énoncé.

--- Examen2-R-H25-Partie1/MainWindow.xaml.cs
+++ Examen2-R-H25-Partie1/MainWindow.xaml.cs
@@ -29,6 +29,10 @@ namespace Examen2_R_H25_Partie1
                 new Employe("12345", "Smith", "Bob", "819-555-5555", "2222", "bob.smith@cegep.com", (float)45.50),
                 new Employe("12346", "Tremblay", "Ginette", "819-555-5555", "1122", "ginette.tremblay@cegep.com", 60)
             };
+            // TODO : Afficher la liste d'employés
         }
+
+        // TODO : Gérer le bouton Ajouter pour qu'il ajoute un employé dans la liste
+        // le nouvel employé doit aussi s'afficher dans la ListBox sur l'interface.
     }
 }
